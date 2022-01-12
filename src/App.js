26c82d2056c2166612731a5df2e430b7fd639c8f
Lakshmi Vara Prasad Koneru <prasad.koneru@milliporesigma.com>
2022-01-11 18:57:16 -0600

    counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useCallback } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  );
+
+  const onIncrement = useCallback((key) => {
+    // time complexity is high if implemented with both find and splice functions.
+    // so implemented with map function.
+    const newData = data.map(d => {
+      if(d.id === key) {
+        d.value++;
+      } 
+      return d;
+    });
+    setData(newData);
+  }, [data]);
+
+  const onDecrement = useCallback((key) => {
+    const newData = data.map(d => {
+      if(d.id === key) {
+        d.value--;
+      } 
+      return d;
+    });
+    setData(newData);
+  }, [data]);
+
+  const totalComponent = () => {
+    return (
+      <div>
+        Total Counter Values: {data
+        .map(d => d.value)
+        .reduce(
+          (previousValue, currentValue, currentIndex, array) => previousValue + currentValue
+        )}
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map(counter => ( 
+        <Counter 
+          key={counter.id}
+          id={counter.id} 
+          value={counter.value}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          />
+      ))}
+      { totalComponent() }
+    </div>
+  );
+}
+
+const Counter = (props) => {
+  const { id, value, onIncrement, onDecrement } = props;
+  return (
+    <div className="counter">
+      <b>Value-{value}</b>
+      <div className="counter-controls">
+        <button className="button is-danger is-small" onClick={() => onDecrement(id)}>-</button>
+        <button className="button is-success is-small" onClick={() => onIncrement(id)}>+</button>
+      </div>
     </div>
   );
 }
